bbb4bc61088caa392055483530ed2476286ad94e
Sam Ismail <sam@samism.net>
2015-11-07 18:21:05 -0600

    Added ability to minimize the media, geolocation.

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
+var hbs = require('express-hbs');
+var fs = require('fs');
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 var app = express();
+
+//var partialsDir = __dirname + '/views';
+//var bricks = fs.readFileSync(partialsDir + '/bricks.hbs', 'utf8');
+
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -23,7 +24,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -56,5 +56,6 @@ app.use(function(err, req, res, next) {
   });
 });
 
+//app.registerPartial('bricksPartial', bricks);
 
 module.exports = app;
